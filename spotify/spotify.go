df0ae3e9417a04fc24701e3cdf465df70ecad547
Robert <robert.apb@hotmail.com>
2014-10-30 00:20:28 -0400

    Now Authorizable

--- spotify/spotify.go
+++ spotify/spotify.go
@@ -4,20 +4,25 @@
 package spotify
 
 import (
-//	"fmt"
-//	"encoding/json"
-//	"github.com/parnurzeal/gorequest"
+	//	"encoding/json"
+	simplejson "github.com/bitly/go-simplejson"
+	"github.com/parnurzeal/gorequest"
+	"encoding/base64"
+	"fmt"
 )
 
 // Spotify struct which we use
 // to wrap our request operations.
 type Spotify struct {
-	clientID        string
-	clientSecret    string
+	clientID           string
+	clientSecret       string
+	//	redirectURI        string
+	accessToken        string
 }
 
 const (
-//	baseURL = "https://api.spotify.com"
+	//	baseURL     = "https://api.spotify.com"
+	accountsURL = "https://accounts.spotify.com/api/token"
 )
 
 // Creates a New Spotify API object with the
@@ -26,7 +31,63 @@ const (
 // 	spotify.New("XXX","YYY")
 func New(clientID, clientSecret string) Spotify {
 
-	shop := Spotify{clientID: clientID, clientSecret: clientSecret}
+	return createSpotify(clientID, clientSecret)
+}
 
+//
+//// Creates a New Spotify API object with the
+//// clientID, clientSecret and redirectURI
+//// Usage:
+//// 	spotify.New("XXX","YYY", "http://ZZZ")
+//func New(clientID, clientSecret, redirectURI string) Spotify {
+//
+//	return createSpotify(clientID, clientSecret, redirectURI)
+//}
+
+func createSpotify(clientID, clientSecret string) Spotify {
+	shop := Spotify{clientID: clientID, clientSecret: clientSecret}
 	return shop
+}
+
+// Authorizes your application against Spotify
+func (spotify *Spotify) Authorize() (bool, []error) {
+
+	result := false
+
+	request := gorequest.New()
+	request.Post(accountsURL)
+	auth := fmt.Sprintf("Basic %s", spotify.getEncodedKeys())
+	request.Set("Authorization", auth)
+	request.Send("grant_type=client_credentials")
+
+	_, body, errs := request.End()
+
+	js, err := simplejson.NewJson([]byte(body))
+	if err != nil {
+		fmt.Println("[Authorize] Error parsing Json!")
+		errs = []error{err}
+	}
+
+	jsToken, exists := js.CheckGet("access_token")
+
+	if exists {
+		spotify.accessToken, err = jsToken.String()
+		if err != nil {
+			fmt.Println("[Authorize] Error Getting Access Token from Json!")
+			errs = []error{err}
+		}
+		result = true
+	}
+
+	return result, errs
+}
+
+// returns base64 encoded authorization
+// Keys for Spotify.
+func (spotify *Spotify) getEncodedKeys() string {
+
+	data := fmt.Sprintf("%v:%v", spotify.clientID, spotify.clientSecret)
+	encoded := base64.StdEncoding.EncodeToString([]byte(data))
+
+	return encoded;
 }
--- spotify/spotify_test.go
+++ spotify/spotify_test.go
@@ -8,8 +8,10 @@ import (
 
 // Create out api variables for easy access
 const (
-	clientID     = "your-clientID-here"
-	clientSecret = "your-secret-clientSecret-here"
+	clientID     = "756100c43d724ae6b791f7804c82b219"
+	clientSecret = "841a197013f847bca78c01b3b69fc72d"
+	redirectURI  = "http://localhost/callback"
+
 )
 
 var spotify = New(clientID, clientSecret)
@@ -19,5 +21,25 @@ func TestNew(t *testing.T) {
 
 	assert.T(t, spotify.clientID == clientID, "clientID should be the same")
 	assert.T(t, spotify.clientSecret == clientSecret, "clientSecret should be the same")
+}
+
+// Should create a new Spotify Object.
+func TestAuthorize(t *testing.T) {
+
+	result, err := spotify.Authorize();
+	assert.T(t, result, "should be true")
+	assert.T(t, len(err) == 0, "should be nil")
+
+	assert.T(t, len(spotify.accessToken) > 0, "should not be nil")
+
+}
+
+// Should create a new Spotify Object.
+func TestGetEncodedKeys(t *testing.T) {
+
+
+	result := spotify.getEncodedKeys();
+	assert.T(t, len(result) > 0, "shouldn't be null")
+
 }
 
